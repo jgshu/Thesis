5cc9d89af1ac5df6f4db14135c50023022875a53
Gavin Wong <gavinv0701@gmail.com>
2021-05-13 15:54:38 +0800

    11th commit

--- model.py
+++ model.py
@@ -84,6 +84,53 @@ class BiLSTM(nn.Module):
         x = lstm_out.contiguous().view(batch_size, -1)
         return self.linear(x)
 
+
+###############
+# Conv LSTM with Attention
+###############
+class ConvLSTM_with_Attion(nn.Module):
+    def __init__(self, n_features, n_hidden, seq_len, n_layers, out_features, do=0.5, device='cpu'):
+        super(ConvLSTM_with_Attion, self).__init__()
+        self.input_dim = n_features
+        self.output_dim = out_features
+        self.hidden_dim = n_hidden
+        self.seq_len = seq_len
+        self.num_layers = n_layers
+        self.drop_out = do
+        self.device = device
+
+        self.conv1 = nn.Sequential(
+            nn.Conv
+        )
+
+        # x: (batch_dim, seq_dim, feature_dim) or (samples, timesteps, features) when considering batch_first=True
+        self.lstm = nn.LSTM(
+            input_size=self.input_dim,
+            hidden_size=self.hidden_dim,
+            num_layers=self.num_layers,
+            dropout=self.drop_out,
+            batch_first=True,
+            bidirectional=True,
+        )
+        self.linear = nn.Linear(in_features=self.hidden_dim * self.seq_len * 2, out_features=self.output_dim)
+
+    def init_hidden(self, batch_size):
+        # even with batch_first = True this remains same as docs
+        hidden_state = torch.zeros(self.num_layers*2, batch_size, self.hidden_dim).requires_grad_().to(self.device)
+        cell_state = torch.zeros(self.num_layers*2, batch_size, self.hidden_dim).requires_grad_().to(self.device)
+        return (hidden_state, cell_state)
+
+    def forward(self, x):
+        batch_size, _, _ = x.shape
+        self.hidden = self.init_hidden(batch_size)
+        lstm_out, _ = self.lstm(
+            x,
+            self.hidden
+        )
+        x = lstm_out.contiguous().view(batch_size, -1)
+        return self.linear(x)
+
+
 ###############
 # simpleLSTM
 ###############
